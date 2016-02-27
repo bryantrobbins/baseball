74e5c3a25e8c77330750c9a0bc5f0feebb62a7ca
Stephen Mele <Smelecs@gmail.com>
2016-02-27 14:33:02 -0500

    added a header and a body and deleted a bunch of base files

--- ui/app/components/version/interpolate-filter.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-export default angular.module('app.version.interpolate-filter', [])
-
-.filter('interpolate', ['version', function(version) {
-  return function(text) {
-    return String(text).replace(/\%VERSION\%/mg, version);
-  };
-}]).name;
