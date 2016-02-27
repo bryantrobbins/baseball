29ae4d2793e5374592fc3066f55c51db0f085ccf
Bryan Robbins <bryan.robbins@finra.org>
2016-02-27 17:01:40 -0500

    Add ui deploy script

--- ui/build/deploy.sh
+++ ui/build/deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 # Import docker image with tag
-docker import - baseball/ui:latest < frontend.tar
-docker run --rm --name ui00 baseball/ui:latest
+docker stop ui00
+docker rmi bryantrobbins/ui
+docker rm ui00
+docker import - bryantrobbins/ui < frontend.tar
+docker run -d --name ui00 -p 80:80 baseball/ui:latest /usr/sbin/nginx -g 'daemon off;'
