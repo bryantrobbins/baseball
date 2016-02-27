bc1ede38583c010f904ff25264f661d6e1864ad2
Bryan Robbins <bryan.robbins@finra.org>
2016-02-27 10:57:34 -0500

    Rename worker dir; add worker-image job to jenkins

--- jobs/build.groovy
+++ jobs/build.groovy
@@ -1,12 +1,18 @@
 job('r-baseball') {
     scm {
       git('git://github.com/bryantrobbins/r-baseball') { node ->
-        node / gitConfigName('Jenkins Auto')
+        node / gitConfigName('Baseball Jenkins Auto')
         node / gitConfigEmail('bryantrobbins@gmail.com')
       }
     }
     steps {
        shell('chmod 700 *.sh; ./build.sh') 
+    }
+}
+
+job('worker-image') {
+    steps {
+       shell('pushd worker; chmod 700 *.sh; ./import.sh') 
     }
 }
 
