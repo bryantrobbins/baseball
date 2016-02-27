299b948a67040cbd5c126bf007380ab5d274c1cb
Bryan Robbins <bryan.robbins@finra.org>
2016-02-26 23:45:42 -0500

    Trying to set git user options

--- jobs/build.groovy
+++ jobs/build.groovy
@@ -1,6 +1,9 @@
 job('r-baseball') {
     scm {
-        git('git://github.com/bryantrobbins/r-baseball')
+      git('git://github.com/bryantrobbins/r-baseball') { node ->
+        node / gitConfigName('Jenkins Auto')
+        node / gitConfigEmail('bryantrobbins@gmail.com')
+      }
     }
     steps {
        shell('chmod *.sh; ./build.sh') 
