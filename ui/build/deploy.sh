98001e82729ff3361193d8c636c555abfb4a9429
Bryan Robbins <bryan.robbins@finra.org>
2016-02-27 17:39:25 -0500

    Use consistent tag

--- ui/build/deploy.sh
+++ ui/build/deploy.sh
@@ -4,5 +4,5 @@
 docker stop ui00
 docker rmi bryantrobbins/ui
 docker rm ui00
-docker import - bryantrobbins/ui < frontend.tar
+docker import - baseball/ui:latest < frontend.tar
 docker run -d --name ui00 -p 80:80 baseball/ui:latest /usr/sbin/nginx -g 'daemon off;'
