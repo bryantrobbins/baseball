df09c3928e1d4df687a8ea3e3d9bf3525165ff24
Bryan Robbins <bryantrobbins@gmail.com>
2016-03-09 06:22:28 +0000

    Add back all build steps; add version to packer vars

--- ui/build/build.sh
+++ ui/build/build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -v
 
 # Add js build steps here (npm, etc.)
-#npm install
-#npm run build
+npm install
+npm run build
 
 # Fetch AWS ECR variables
-#chmod 700 fetch.sh
-#./fetch.sh > variables.json
+version=$1
+chmod 700 fetch.sh
+./fetch.sh $version > variables.json
 
 # Build docker image with packer
 packer="/usr/local/bin/packer"
