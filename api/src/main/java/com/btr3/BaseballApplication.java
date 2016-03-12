6428ae7c06ca44fdce4a01bc4802889d810d09b0
SMxJrz <Smelecs@gmail.com>
2016-03-12 14:25:23 -0500

    Base project for api

---
+++ api/src/main/java/com/btr3/BaseballApplication.java
@@ -0,0 +1,12 @@
+package com.btr3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseballApplication.class, args);
+	}
+}
