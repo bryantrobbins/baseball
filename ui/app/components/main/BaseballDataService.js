524f4e4b52b91af2fd0abfae9f9af4eb3112fcc0
JacobSheppard <jshep409@gmail.com>
2016-03-12 22:22:53 -0500

    Adding a controller to serve up the metadata and adding code into services/controllers (commented out for now) that queries these endpoints.

--- ui/app/components/main/BaseballDataService.js
+++ ui/app/components/main/BaseballDataService.js
@@ -1,16 +1,31 @@
+import $q from 'q';
 const HTTP = Symbol();
 
 class BaseballDataService{
-    constructor($http){
+    constructor($http,$resource){
         this[HTTP] = $http;
+
+		//Add back in when httpBackend is switched to Boot
+		/**this.tables = $resource('http://localhost:8004/tables',{},{
+			query: {method: 'get', isArray: true}
+		});
+		this.metadata = $resource('http://localhost:8004/metadata',{},{
+			query: {method: 'get', isArray: false}
+		});**/
     }
 
     getTables(){
-        return this[HTTP].get('/getDataSetNames');
+	    return this[HTTP].get('/getDataSetNames');	
+        //return this.tables.query();
     }
 
     getTableMetadata(table){
+
         return this[HTTP].get('/getDataSetMetadata');
+
+		//return $q.resolve(this.metadata.query());
+
+		
     }
 }
 
