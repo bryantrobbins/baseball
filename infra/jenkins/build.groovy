bef3abd082627b2f6af9b6285098409718413f3b
Bryan Robbins <bryantrobbins@gmail.com>
2016-03-19 11:53:56 -0400

    Add api args to dev

--- infra/jenkins/build.groovy
+++ infra/jenkins/build.groovy
@@ -41,10 +41,30 @@ job('ui-image') {
     }
 }
 
+job('api-image') {
+    wrappers {
+        preBuildCleanup()
+    }
+    scm {
+        git('git://github.com/bryantrobbins/baseball') { node ->
+            node / gitConfigName('Baseball Jenkins Auto')
+            node / gitConfigEmail('bryantrobbins@gmail.com')
+            wipeOutWorkspace(false)
+        }
+    }
+    steps {
+       shell('pushd api/build; chmod 700 *.sh; now=`date +%Y%m%d%H%M%S`; ./build.sh $now')
+    }
+    publishers {
+    }
+}
+
 job('deploy-DEV') {
     parameters {
       textParam('UI_DESIRED_COUNT', '1', 'Number of UI containers to deploy')
       textParam('UI_VERSION', 'latest', 'Version of UI container to deploy')
+      textParam('API_DESIRED_COUNT', '1', 'Number of API containers to deploy')
+      textParam('API_VERSION', 'latest', 'Version of API container to deploy')
     }
     wrappers {
         preBuildCleanup()
@@ -56,7 +76,7 @@ job('deploy-DEV') {
         }
     }
     steps {
-       shell('pushd infra/stacks; chmod 700 *.sh; echo "UIDesiredCount=${UI_DESIRED_COUNT}" >> dev.properties; echo "UIVersion=${UI_VERSION}" >> dev.properties; ./update.sh dev')
+       shell('pushd infra/stacks; chmod 700 *.sh; echo "UIDesiredCount=${UI_DESIRED_COUNT}" >> dev.properties; echo "UIVersion=${UI_VERSION}" >> dev.properties; echo "APIDesiredCount=${API_DESIRED_COUNT}"; echo "APIVersion=${API_Version}"; ./update.sh dev')
     }
     publishers {
     }
