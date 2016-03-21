80c4d0df420400e3e50c01beafd248c6cc59f441
Bryan Robbins <bryantrobbins@gmail.com>
2016-03-20 20:46:59 -0400

    Comment out data extract for now

--- worker/build/build.sh
+++ worker/build/build.sh
@@ -1,17 +1,16 @@
 #!/bin/bash -v
 
 # Add R build steps here (if any)
-
-pushd extract
-./extract.sh
-popd
+#pushd extract
+#./extract.sh
+#popd
 #Rscript Build.R
 
 # Fetch AWS ECR variables
-#version=$1
-#chmod 700 fetch.sh
-#./fetch.sh $version > variables.json
+version=$1
+chmod 700 fetch.sh
+./fetch.sh $version > variables.json
 
 # Build docker image with packer
-#packer="/usr/local/bin/packer"
-#$packer build -var-file=variables.json worker.json
+packer="/usr/local/bin/packer"
+$packer build -var-file=variables.json worker.json
