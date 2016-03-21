10c459335ffacbdb44d1e39be71f18316ace5804
Bryan Robbins <bryantrobbins@gmail.com>
2016-03-20 20:34:57 -0400

    Move cleanup into extract

--- worker/extract/extract.sh
+++ worker/extract/extract.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-./clean.sh
+echo "Cleaning old files"
+rm -rf lahman gamelogs tmp_*
 
 echo "Download groovy (temp)"
 wget -O groovy.jar http://search.maven.org/remotecontent?filepath=org/codehaus/groovy/groovy/2.4.6/groovy-all-2.4.6.jar
