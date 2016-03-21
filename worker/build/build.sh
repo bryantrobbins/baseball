10d7b57d56b59134ec28e91d42a04fed877ae2b9
Bryan Robbins <bryantrobbins@gmail.com>
2016-03-20 20:30:12 -0400

    Merging package and image builds into single build.sh

--- worker/build/build.sh
+++ worker/build/build.sh
@@ -2,11 +2,14 @@
 
 # Add R build steps here (if any)
 
+pushd ../extract
+./extract.sh
+
 # Fetch AWS ECR variables
-version=$1
-chmod 700 fetch.sh
-./fetch.sh $version > variables.json
+#version=$1
+#chmod 700 fetch.sh
+#./fetch.sh $version > variables.json
 
 # Build docker image with packer
-packer="/usr/local/bin/packer"
-$packer build -var-file=variables.json worker.json
+#packer="/usr/local/bin/packer"
+#$packer build -var-file=variables.json worker.json
