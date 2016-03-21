51483c1940156057733a12e577ae06290b641cbf
Bryan Robbins <bryantrobbins@gmail.com>
2016-03-20 21:28:04 -0400

    Rolling back some R integration to facilitate testing of ecs api calls

--- worker/build/staging/entry.sh
+++ worker/build/staging/entry.sh
@@ -3,5 +3,9 @@
 outfile=$1
 pushd /workdir
 echo $(base64 -d <<< $2) > config.json
-Rscript generic.R
+#Rscript generic.R
+# For testing, just copy input as output
+# This output should really be created by the R script
+mv config.json output.csv
 aws s3 cp output.csv --acl public-read s3://baseball-workbench/$1
+
