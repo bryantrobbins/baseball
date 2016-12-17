30ae5265b0c6a119dd4d9dd72d7baa10e17b3d2b
Bryan Robbins <bryantrobbins@gmail.com>
2016-12-17 18:14:36 -0500

    Remove packer steps from ui build

--- ui/build/build.sh
+++ ui/build/build.sh
@@ -3,12 +3,3 @@
 # Add js build steps here (npm, etc.)
 npm install
 npm run build
-
-# Fetch AWS ECR variables
-version=$1
-chmod 700 fetch.sh
-./fetch.sh $version > variables.json
-
-# Build docker image with packer
-packer="/usr/local/bin/packer"
-$packer build -var-file=variables.json frontend.json
