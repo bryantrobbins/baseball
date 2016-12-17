2295b8d61052393bda66659afbe6e3462587db20
Bryan Robbins <bryantrobbins@gmail.com>
2016-12-17 17:09:49 -0500

    Add packer download to build scr ipt

--- worker/build/build.sh
+++ worker/build/build.sh
@@ -7,11 +7,17 @@
 #Rscript Build.R
 #mv baseball/staging
 
+# Install packer
+pversion="0.12.1"
+purl="https://releases.hashicorp.com/packer/${pversion}/packer_${pversion}_linux_amd64.zip"
+wget $purl
+unzip packer_${pversion}_linux_amd64.zip
+
 # Fetch AWS ECR variables
 version=$1
 chmod 700 fetch.sh
 ./fetch.sh $version > variables.json
 
 # Build docker image with packer
-packer="/usr/local/bin/packer"
+packer="./packer"
 $packer build -var-file=variables.json worker.json
