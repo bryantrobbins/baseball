fb58fa29e71207199e37387af8562fe4f3a0011c
Bryan Robbins <bryantrobbins@gmail.com>
2016-12-17 20:57:59 -0500

    Delete some stuff for now that is probably not needed for v1

--- api/README.md
+++
@@ -1,3 +0,0 @@
-This is the Application Programming Inteface (API) or "back-end" component.
-
-This will provide a sexy Restful API for querying baseball statistics, which means it will interact with raw data in various formats behind the scenes.
--- api/testing/play.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cluster="BTR-standard-ECSCluster-PCPU2E2BPZZQ"
-definition="arn:aws:ecs:us-east-1:235079478010:task-definition/baseball-dev-WorkerTaskDefinition-963P1KUD9Z9O:1"
-aws ecs run-task --task-definition ${definition} --cluster ${cluster}
