fcd73631ce93587b8c5a86ba152bafeb74fc1b01
Bryan Robbins <bryantrobbins@gmail.com>
2016-12-17 22:44:08 -0500

    Add command to task def; Update entry.sh; make entry.sh exec

--- worker/build/staging/entry.sh
+++ worker/build/staging/entry.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-outfile=$1
 pushd /workdir
-echo $(base64 -d <<< $2) > config.json
 #Rscript generic.R
-# For testing, just copy input as output
 # This output should really be created by the R script
-mv config.json output.csv
-aws s3 cp output.csv --acl public-read s3://baseball-workbench/$1
-
+echo "Hello world" >> output.csv
+aws s3 cp output.csv s3://baseball-workbench-data/output.csv
