e533894d97d6b185db00771386d0a05868bb1225
Bryan Robbins <bryantrobbins@gmail.com>
2016-12-19 00:50:17 -0500

    new python script for worker ... getting message from queue and whatnon

--- worker/build/staging/entry.sh
+++ worker/build/staging/entry.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 pushd /workdir
-#Rscript generic.R
-# This output should really be created by the R script
 echo "Hello world" >> output.csv
 aws s3 cp output.csv s3://baseball-workbench-data/output.csv
---
+++ worker/build/staging/job.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+import boto3
+import json
+import os
+import time
+from boto3.dynamodb.conditions import Key, Attr
+from botocore.exceptions import ClientError
+
+def extractSingleJob(jobId):
+    print('Processing job {}'.format(jobId))
+    try:
+        response = dynamo.get_item(
+            Key={
+                'job-id': jobId
+            }
+        )
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+        return None
+    else:
+        item = response['Item']
+        return item
+
+print('Starting job')
+
+# Temp for testing; will come from job config
+os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
+os.environ["JOB_TABLE"] = "baseball-jobs"
+os.environ["JOB_QUEUE"] = "baseball-jobs-queue"
+
+# Load vars
+jobTable = os.environ['JOB_TABLE']
+jobQueue = os.environ['JOB_QUEUE']
+
+# Init clients
+dynamo = boto3.resource('dynamodb').Table(jobTable)
+queue = boto3.resource('sqs').get_queue_by_name(QueueName=jobQueue)
+
+while True:
+    messages = queue.receive_messages()
+    if (len(messages) == 0):
+        print('There are no messages; sleeping')
+        time.sleep(10)
+    else:
+        print('Processing {} messages'.format(len(messages)))
+        for message in messages:
+            jobInfo = extractSingleJob(message.body)
+            if jobInfo is not None:
+                # Call R
+                # Do upload
+                # Update DB entry
+                # Delete message                
+                print('Doing the work')
