a8f3417ec7942b0ba386dbb9cbeed3824618552a
Bryan Robbins <bryantrobbins@gmail.com>
2016-12-24 10:54:27 -0600

    Update standard stack to support sqs + dynamo usage; continue job py script

--- infra/stacks/standard/create.sh
+++ infra/stacks/standard/create.sh
@@ -3,6 +3,16 @@
 # Get options
 tname="cloud.json"
 tfile="$(cd "$(dirname "${tname}")"; pwd)/$(basename "${tname}")"
+
+# Determine if running on Windows (affects template file argument to aws cli)
+platform=`uname`
+if [[ ${platform} == *"MINGW"* ]]; then
+  echo "Using Windows file path"
+  tfile=`cygpath -w ${tfile} | sed -e 's/[\/]/\/\//g'`
+else
+  echo "Using Linux file path"
+fi
+
 kname="builder"
 
 # Delete old keypair
@@ -12,7 +22,7 @@ aws ec2 delete-key-pair --key-name ${kname} --region us-east-1
 aws ec2 create-key-pair --key-name ${kname} --output text --region us-east-1 | sed 's/.*BEGIN.*-$/-----BEGIN RSA PRIVATE KEY-----/' | sed "s/.*${kname}$/-----END RSA PRIVATE KEY-----/" > ${kname}.pem
 chmod 600 ${kname}.pem
 
-cmd="aws cloudformation create-stack --stack-name BTR-standard --template-body file:///${tfile} --capabilities CAPABILITY_IAM --region us-east-1 --parameters ParameterKey=KeyName,ParameterValue=${kname}"
+cmd="aws cloudformation create-stack --stack-name BTR-standard --template-body \"file://${tfile}\" --capabilities CAPABILITY_IAM --region us-east-1 --parameters ParameterKey=KeyName,ParameterValue=${kname}"
 
 # Execute cmd
 eval $cmd
