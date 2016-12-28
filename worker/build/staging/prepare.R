2bec2c1381c4743992597d274257720a93dc19b6
Bryan Robbins <bryantrobbins@gmail.com>
2016-12-28 02:43:01 -0500

    Ignore some local working dirs; remove unused R package from prepare script

--- worker/build/staging/prepare.R
+++ worker/build/staging/prepare.R
@@ -1,6 +1,6 @@
 cat('Installing packages\n')
 cran <- "http://cran.rstudio.com/"
 libloc <- "."
-plist <- c("plyr", "jsonlite")
+plist <- c("plyr")
 install.packages(plist, repos=cran, lib=libloc)
 cat('DONE\n')
