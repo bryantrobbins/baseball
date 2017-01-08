7f44839c811bc024ebfbf9518a2c6360b70694ba
Bryan Robbins <bryantrobbins@gmail.com>
2017-01-08 18:11:14 -0500

    Deploy zip to both lambda functions

--- shared/deploy.sh
+++ shared/deploy.sh
@@ -2,4 +2,5 @@
 
 cd packages
 aws s3 cp btr3baseball.zip s3://baseball-workbench-builds/btr3baseball/btr3baseball-${CODEBUILD_BUILD_ID}.zip
-aws lambda update-function-code --s3-bucket baseball-workbench-builds --s3-key btr3baseball-${CODEBUILD_BUILD_ID}.zip
+aws lambda update-function-code --function-name baseball-submitJob --s3-bucket baseball-workbench-builds --s3-key btr3baseball-${CODEBUILD_BUILD_ID}.zip
+aws lambda update-function-code --function-name baseball-getJobInfo --s3-bucket baseball-workbench-builds --s3-key btr3baseball-${CODEBUILD_BUILD_ID}.zip
