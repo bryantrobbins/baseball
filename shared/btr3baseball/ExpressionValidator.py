fb495cf7c18a21a80dc3289ba910f9fd5c2a6d3c
Bryan Robbins <bryantrobbins@gmail.com>
2017-01-21 21:21:21 -0500

    Working on ASTs

--- shared/btr3baseball/ExpressionValidator.py
+++ shared/btr3baseball/ExpressionValidator.py
@@ -1,35 +1,35 @@
 from pyparsing import Literal,CaselessLiteral,Word,Combine,Group,Optional,\
     ZeroOrMore,Forward,nums,alphas,ParseException
 
-point = Literal( "." )
-fnumber = Combine( Word( "+-"+nums, nums ) + Optional( point + Optional( Word( nums ) ) ) )
-quote = Literal("'").suppress()
-comma = Literal(",")
-ident = Word(alphas)
-plus  = Literal( "+" )
-minus = Literal( "-" )
-mult  = Literal( "*" )
-div   = Literal( "/" )
-lpar  = Literal( "(" )
-rpar  = Literal( ")" )
-num_const = fnumber
-str_const = quote + ident + quote
-addop  = plus | minus
-multop = mult | div
-expop = Literal( "^" )
-
-expr = Forward()
-col = ident
-func = ident + lpar + Optional( expr + ZeroOrMore( (comma + expr) ) ) + rpar
-atom = Optional('-') + ( num_const | func | col )
-parfactor = ( atom | lpar + atom + rpar )
-factor = Forward()
-factor << ( parfactor + ZeroOrMore(expop + parfactor) )
-term = factor + ZeroOrMore( ( multop + factor ))
-expr << (term + ZeroOrMore( ( addop + term ))  | str_const)
 
 class ExpressionValidator:
     def __init__(self):
+        point = Literal( "." )
+        fnumber = Combine( Word( "+-"+nums, nums ) + Optional( point + Optional( Word( nums ) ) ) )
+        quote = Literal("'")
+        comma = Literal(",").suppress()
+        ident = Word(alphas)
+        plus  = Literal( "+" )
+        minus = Literal( "-" )
+        mult  = Literal( "*" )
+        div   = Literal( "/" )
+        lpar  = Literal( "(" )
+        rpar  = Literal( ")" )
+        colmarker = Literal('$')
+        addop  = plus | minus
+        multop = mult | div
+        expop = Literal( "^" )
+
+        expr = Forward()
+        term = Forward()
+        factor = Forward()
+        str_const = ( quote + ident + quote ).setParseAction(Str)
+        col = ( colmarker + lpar + str_const + rpar ).setParseAction(Col)
+        func = ( ident + lpar + Optional( expr + ZeroOrMore( (comma + expr) ) ) + rpar ).setParseAction(Func)
+        atom = ( Optional('-') + ( ( fnumber | func | col ) | ( lpar + expr + rpar ) ) ).setParseAction(Atom)
+        factor <<  ( ( atom + expop + factor ) | atom ).setParseAction(Factor)
+        term << ( ( factor + multop + term ) | factor ).setParseAction(Term)
+        expr << ( ( term + addop + expr ) | term | str_const ).setParseAction(Expr)
         self.grammar = expr
 
     def parseExpression(self, strEx):
@@ -51,4 +51,62 @@ class ExpressionValidatorResult:
             return self.tokens.__str__()
         if self.message != None:
             return 'In expression "{}": {}'.format(self.expression, self.message)
+
+class ASTNode(object):
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.assignFields()
+    def __str__(self):
+        return self.__class__.__name__ + ':' + str(self.__dict__)
+    __repr__ = __str__
+
+class Col(ASTNode):
+    def assignFields(self):
+        self.name = self.tokens[2]
+        del self.tokens
+
+class Func(ASTNode):
+    def assignFields(self):
+        self.name = self.tokens[0]
+        self.argList = self.tokens[2:-1]
+        del self.tokens
+
+class Str(ASTNode):
+    def assignFields(self):
+        self.body = self.tokens[1]
+        del self.tokens
+
+class Atom(ASTNode):
+    def assignFields(self):
+        self.negate = False
+        if (self.tokens[0] == '-'):
+            self.negate = True
+            self.body = self.tokens[1]
+        else:
+            self.body = self.tokens[0]
+        del self.tokens
+
+class Factor(ASTNode):
+    def assignFields(self):
+        self.latom = self.tokens[0]
+        if (len(self.tokens) > 1):
+            self.op = self.tokens[1]
+            self.rfactor = self.tokens[2]
+        del self.tokens
+
+class Term(ASTNode):
+    def assignFields(self):
+        self.lfactor = self.tokens[0]
+        if (len(self.tokens) > 1):
+            self.op = self.tokens[1]
+            self.rterm = self.tokens[2]
+        del self.tokens
+
+class Expr(ASTNode):
+    def assignFields(self):
+        self.lterm = self.tokens[0]
+        if (len(self.tokens) > 1):
+            self.op = self.tokens[1]
+            self.rexpr = self.tokens[2]
+        del self.tokens
 
--- shared/btr3baseball/tests.py
+++ shared/btr3baseball/tests.py
@@ -2,17 +2,31 @@ from ExpressionValidator import ExpressionValidator
 import unittest
 
 vv = ExpressionValidator()
+#ee = [
+#    "2 * $('HR')",
+#    "2",
+#		"'BRYAN'",
+#		"hi(2) * $('HR')",
+#		"hi(2,3,4)",
+#		"-hi(2,3,4)",
+#		"-hi((2),log(-$('HR')),4)",
+#		"hi(2)^-5^6",
+#		"(hi(2))^-5",
+#]
+
 ee = [
-    "2 * COL('HR')",
-    "2",
-		"'BRYAN'",
-		"hi(2) * HR",
-		"hi(2,3,4)",
-		"-hi(2,3,4)",
-		"-hi((2),log(-col('HR')),4)",
-		"hi(2)^-5^6",
-		"(hi(2))^-5",
-]
+"2",
+"3.14",
+"'Hey'",
+"-2",
+"2*3",
+"2*3*4",
+"hi(2)",
+"hi(2,3)",
+#"$('HR')",
+#"2 * $('HR')",
+#"2 * 3 * $('HR')"
+] 
 
 for ex in ee:
     result = vv.parseExpression(ex)
