eae23cd78d8e3974b219d1dc3653fa556c4a651f
Bryan Robbins <bryantrobbins@gmail.com>
2017-01-25 12:01:18 -0600

    Add assertions for first few tests

--- shared/btr3baseball/TestExpressionValidator.py
+++ shared/btr3baseball/TestExpressionValidator.py
@@ -12,24 +12,69 @@ class TestExpressionValidator(unittest.TestCase):
     def helper_testString(self, ex):
         validator = self.createSimpleValidator()
         result = validator.validateExpression(ex)
-        print('{} => {}'.format(ex, result))
+        #print('{} => {}'.format(ex, result))
         return result
+
+    def helper_assertAstTypeAndNext(self, ast, typeName, attrNext):
+        self.assertEqual(typeName, ast.__class__.__name__)
+        self.assertTrue(hasattr(ast, attrNext))
+        return ast.__getattribute__(attrNext)
+
+    def helper_assertAstTypeAndValue(self, ast, typeName, attr, attrVal):
+        self.assertEqual(typeName, ast.__class__.__name__)
+        self.assertTrue(hasattr(ast, attr))
+        self.assertEqual(attrVal, ast.__getattribute__(attr))
 
     def testInteger(self):
         result = self.helper_testString("2")
+        res = self.helper_assertAstTypeAndNext(result.ast, "Expr", 'value')
+        res = self.helper_assertAstTypeAndNext(res, "NumExpr", "lterm")
+        res = self.helper_assertAstTypeAndNext(res, "Term", "lfactor")
+        res = self.helper_assertAstTypeAndNext(res, "Factor", "latom")
+        self.helper_assertAstTypeAndValue(res, "Atom", "negate", False)
+        self.helper_assertAstTypeAndValue(res, "Atom", "value", '2')
 
     def testFloat(self):
         result = self.helper_testString("3.14")
+        res = self.helper_assertAstTypeAndNext(result.ast, "Expr", 'value')
+        res = self.helper_assertAstTypeAndNext(res, "NumExpr", "lterm")
+        res = self.helper_assertAstTypeAndNext(res, "Term", "lfactor")
+        res = self.helper_assertAstTypeAndNext(res, "Factor", "latom")
+        self.helper_assertAstTypeAndValue(res, "Atom", "negate", False)
+        self.helper_assertAstTypeAndValue(res, "Atom", "value", '3.14')
 
     def testString(self):
         result = self.helper_testString("'Hey'")
+        res = self.helper_assertAstTypeAndNext(result.ast, "Expr", 'value')
+        self.helper_assertAstTypeAndValue(res, "Str", "value", 'Hey')
 
     def testNegativeInt(self):
         result = self.helper_testString("-2")
+        res = self.helper_assertAstTypeAndNext(result.ast, "Expr", 'value')
+        res = self.helper_assertAstTypeAndNext(res, "NumExpr", "lterm")
+        res = self.helper_assertAstTypeAndNext(res, "Term", "lfactor")
+        res = self.helper_assertAstTypeAndNext(res, "Factor", "latom")
+        self.helper_assertAstTypeAndValue(res, "Atom", "negate", True)
+        self.helper_assertAstTypeAndValue(res, "Atom", "value", '2')
 
     def testMult(self):
         result = self.helper_testString("2*3")
+        res = self.helper_assertAstTypeAndNext(result.ast, "Expr", 'value')
+        res = self.helper_assertAstTypeAndNext(res, "NumExpr", "lterm")
 
+        self.helper_assertAstTypeAndValue(res, "Term", "op", "*")
+        leftres = self.helper_assertAstTypeAndNext(res, "Term", "lfactor")
+        rightres = self.helper_assertAstTypeAndNext(res, "Term", "rterm")
+
+        res = self.helper_assertAstTypeAndNext(leftres, "Factor", "latom")
+        self.helper_assertAstTypeAndValue(res, "Atom", "negate", False)
+        self.helper_assertAstTypeAndValue(res, "Atom", "value", '2')
+
+        res = self.helper_assertAstTypeAndNext(rightres, "Term", "lfactor")
+        res = self.helper_assertAstTypeAndNext(res, "Factor", "latom")
+        self.helper_assertAstTypeAndValue(res, "Atom", "negate", False)
+        self.helper_assertAstTypeAndValue(res, "Atom", "value", '3')
+        
     def testMultAndAdd(self):
         result = self.helper_testString("2*3 + 5*4")       
 
